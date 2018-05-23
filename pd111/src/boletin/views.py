cfa8e1e7e5bf43a8f98af024fa466a883e115b4f
Unknown <AlbertoASR27@hotmail.com>
2018-05-23 22:02:02 +0200

    Video 18
    Contexto en la Vista

--- pd111/src/boletin/views.py
+++ pd111/src/boletin/views.py
@@ -9,11 +9,14 @@ from .models import Registrado
 # Create your views here.
 
 def inicio(request):
+	titulo = "HOLA"
+	if request.user.is_authenticated():
+		titulo = "Bienvenido %s" %(request.user)
 	form = RegForm(request.POST or None)
 	if form.is_valid():
 		form_data = form.cleaned_data
 		abc = form_data.get("email")
 		abc2 = form_data.get("nombre")
 		obj = Registrado.objects.create(email=abc, nombre=abc2)
-	context = {"el_form": form,}
+	context = {"el_form": form, "titulo": titulo,}
 	return render(request, "inicio.html", context)
