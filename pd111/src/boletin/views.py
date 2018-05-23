b9d2fb68e1ff8c29223ca6e41ace5de13553b2fe
Unknown <AlbertoASR27@hotmail.com>
2018-05-23 20:40:55 +0200

    Video 15
    Guardar datos de formulario

--- pd111/src/boletin/views.py
+++ pd111/src/boletin/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from .forms import RegForm
+from .models import Registrado
 
 # Create your views here.
 
@@ -11,7 +12,8 @@ def inicio(request):
 	form = RegForm(request.POST or None)
 	if form.is_valid():
 		form_data = form.cleaned_data
-		print form_data.get("nombre")
-		print form_data.get("edad")
+		abc = form_data.get("email")
+		abc2 = form_data.get("nombre")
+		obj = Registrado.objects.create(email=abc, nombre=abc2)
 	context = {"el_form": form,}
 	return render(request, "inicio.html", context)
