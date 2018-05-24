d616b7bdc16766ee510f982fb2dc72654557eeab
Unknown <AlbertoASR27@hotmail.com>
2018-05-24 03:47:11 +0200

    Video 23
    Configuracion Bootstrap

--- pd111/src/pd111/settings.py
+++ pd111/src/pd111/settings.py
@@ -128,10 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_URL = '/media/'
+MEDIA_URL = '/media/'
 
 
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static_pro", "static"),
     # '/var/www/static',
 ]
